db6cb92aa5dd06223a2b670f7c6efaa3b77b1b61
guido <guidogelman@gmail.com>
2010-03-23 12:32:17 -0400

    add validations to models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,9 @@ class Customer < Sequel::Model
   # set_dataset dataset.sequence(:seq_customers_id)
   one_to_many :items
   one_to_many :califications
+
+  def validate
+    error.add(:nickname, :not_present) if nickname.nil?
+    error.add(:email, :not_present) if email.nil?
+  end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,8 @@
 class Site < Sequel::Model
   one_to_many :items
+
+  def validate
+    errors.add(:site_id, :not_present) if site_id.nil?
+    errors.add(:locale, :not_present) if locale.nil?
+  end
 end
