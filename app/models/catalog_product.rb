610445a7d0c694267f889c37698f7b9208c8c038
guido <guidogelman@gmail.com>
2010-03-30 09:22:10 -0400

    fix methods

--- app/models/catalog_product.rb
+++ app/models/catalog_product.rb
@@ -4,17 +4,38 @@ class CatalogProduct < Sequel::Model
   one_to_many :reviews
 
 
-  def calculate_reviews_summary 
+  # def calculate_reviews_summary 
+    # suma = @qty5 = @qty4 = @qty3 = @qty2 = @qty1 = 0;
+    # for review in self.reviews do
+      # eval("@qty" +review.points.to_i.to_s + "+=1")
+      # suma+=review.points
+    # end
+    # @prom = ((suma * 2) / self.reviews.length).round / 2.to_f
+  # end
+
+  # def get_balls_image_link
+    # return @prom.to_s.delete "."
+  # end
+
+  attr_reader :qty5, :qty4, :qty3, :qty2, :qty1, :prom
+
+  def calculate_reviews_summary
     suma = @qty5 = @qty4 = @qty3 = @qty2 = @qty1 = 0;
+
     for review in self.reviews do
       eval("@qty" +review.points.to_i.to_s + "+=1")
       suma+=review.points
     end
+
     @prom = ((suma * 2) / self.reviews.length).round / 2.to_f
   end
 
   def get_balls_image_link
-    return @prom.to_s.delete "."
+    if @prom.to_i != @prom.ceil
+      return @prom.to_s.delete "."
+    else
+      return @prom.to_i
+    end
   end
 
 end
