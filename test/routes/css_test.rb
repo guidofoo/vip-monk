6f71f111e5f35fed094020cc43848ee884a79c4f
ggelman <guidogelman@gmail.com>
2010-04-07 15:46:59 -0400

    Fix build.

--- test/routes/css_test.rb
+++ test/routes/css_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 
 class CssTest < Test::Unit::TestCase
   test "renders the default stylesheet" do
-    get "/css/main.css"
+    get "/css/vip.css"
     assert_equal "text/css;charset=UTF-8", last_response.content_type
   end
 end
